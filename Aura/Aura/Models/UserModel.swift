a81b54ab14137d3bbd3f318dd5084e30e29e956a
cantoch <cantoch@icloud.com>
2025-06-05 04:56:05 +0200

    Avancement réalisé sur la méthode login, parsing, reception token

---
+++ Aura/Aura/Models/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  Aura
+//
+//  Created by Renaud Leroy on 03/06/2025.
+//
+
+struct LoginRequest: Encodable {
+    let username: String
+    let password: String
+}
+
+struct LoginResponse: Codable {
+    let token: String
+}
--- Aura/Aura/ViewModels/AuthenticationViewModel.swift
+++ Aura/Aura/ViewModels/AuthenticationViewModel.swift
@@ -11,14 +11,41 @@ class AuthenticationViewModel: ObservableObject {
     @Published var username: String = ""
     @Published var password: String = ""
     
-    let onLoginSucceed: (() -> ())
+    let onLoginSucceed: (() -> ())   // ?????
     
     init(_ callback: @escaping () -> ()) {
         self.onLoginSucceed = callback
     }
     
     func login() {
-        print("login with \(username) and \(password)")
-        onLoginSucceed()
+        var request = URLRequest(url: URL(string: "http://127.0.0.1:8080/auth")!)
+        request.httpMethod = "POST"
+        
+        let body = LoginRequest(username: username, password: password)
+        
+        let jsonData = try! JSONEncoder().encode(body)
+        request.httpBody = jsonData
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")  // Cela indique a l'API que le corps contient du JSON
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                guard let data = data, error == nil else {
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse,
+                      response.statusCode == 200 else {
+                    print("mauvais statutcode")
+                    return
+                }
+                
+                do {
+                    let tokenResponse = try JSONDecoder().decode(LoginResponse.self, from: data)
+                } catch {
+                }
+            }
+        }
+        .resume()
     }
 }
